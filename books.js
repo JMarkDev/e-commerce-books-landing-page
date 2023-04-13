396bbcc959f8db32fb5f04707acc7f11184d0c9f
josielmark <jmseroy@gmail.com>
2023-04-13 11:15:19 +0800

    dynamic frontend

--- books.js
+++ books.js
@@ -3,21 +3,27 @@
     join() method creates and returns a new string by concatenating all of the elements in an array
 
 */
+let books;
 
-function renderBooks(filter) {
+async function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
 
-    const books = getBooks();
+    booksWrapper.classList += ' books__loading'
+    if (!books) {
+        books = await getBooks(); 
+    }
 
-    if (filter === 'LOW_TO_HIGH') {
-        books.sort((a, b) => a.originalPrice - b.originalPrice);
+    booksWrapper.classList.remove('books__loading')
+    if (!filter || filter === 'DEFAULT') {
+        books.sort((a, b) => a.id- b.id)
+    }
+    else if (filter === 'LOW_TO_HIGH') {
+        books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
     } else if (filter ==='HIGH_TO_LOW') {
-        books.sort((a, b) => b.originalPrice - a.originalPrice);
+        books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
     } else if (filter === 'RATING') {
         books.sort((a, b) => a.rating - b.rating);
     }
-
-
 
     const booksHtml = books.map(book => {
         return  `<div class="book">
@@ -31,7 +37,7 @@ function renderBooks(filter) {
             ${ratingHTML(book.rating)}
         </div>
         <div class="book__price">
-            <span>$${book.originalPrice.toFixed(2)}</span>
+            ${priceHTML(book.originalPrice, book.salePrice)}
         </div>
     </div>`
     })
@@ -39,6 +45,14 @@ function renderBooks(filter) {
 
     booksWrapper.innerHTML = booksHtml;
 };
+
+function priceHTML(originalPrice, salePrice) {
+    if (!salePrice) {
+        return `$${originalPrice.toFixed(2)}`
+    }
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`
+    // <span>$${book.originalPrice.toFixed(2)}</span>
+}
 
 function ratingHTML(rating) {
     let ratingHTML = '';
@@ -63,55 +77,67 @@ setTimeout( () => {
 
 //  FAKE DATA
 function getBooks() {
-    return [
-        {
-            id: 1,
-            title: "Crack the Coding Interview",
-            url: "assets/crack the coding interview.png",
-            originalPrice: 19.95,
-            salePrice: 14.95,
-            rating: 3
-        },
-        {
-            id: 2,
-            title: "Mystery",
-            url: "assets/book-8.jpeg",
-            originalPrice: 29.95,
-            salePrice: 14.95,
-            rating: 2
-        },
-        {
-            id: 3,
-            title: "Deep Work",
-            url: "assets/deep work.jpeg",
-            originalPrice: 4.95,
-            salePrice: 4.95,
-            rating: 5
-        },
-        {
-            id: 4,
-            title: "Your Next Five Moves",
-            url: "assets/book-7.jpg",
-            originalPrice: 59.95,
-            salePrice: 1.95,
-            rating: 5
-        },
-        {
-            id: 5,
-            title: "Power",
-            url: "assets/book-5.jpeg",
-            originalPrice: 39.95,
-            salePrice: 22.95,
-            rating: 4.5
-        },
-        {
-            id: 6,
-            title: "5 Second Rule",
-            url: "assets/book-6.jpeg",
-            originalPrice: 47.95,
-            salePrice: 32.95,
-            rating: 4.5
-        }
+    return new Promise((resolve) => {
+        setTimeout(() =>{
+            resolve([
+            {
+                id: 1,
+                title: "Crack the Coding Interview",
+                url: "assets/crack the coding interview.png",
+                originalPrice: 19.95,
+                salePrice: null,
+                rating: 3
+            },
+            {
+                id: 2,
+                title: "Mystery",
+                url: "assets/book-8.jpeg",
+                originalPrice: 29.95,
+                salePrice: null,
+                rating: 2
+            },
+            {
+                id: 3,
+                title: "Deep Work",
+                url: "assets/deep work.jpeg",
+                originalPrice: 14.95,
+                salePrice: 4.95,
+                rating: 5
+            },
+            {
+                id: 4,
+                title: "Your Next Five Moves",
+                url: "assets/book-7.jpg",
+                originalPrice: 59.95,
+                salePrice: 1.95,
+                rating: 5
+            },
+            {
+                id: 5,
+                title: "Power",
+                url: "assets/book-5.jpeg",
+                originalPrice: 39.95,
+                salePrice: 22.95,
+                rating: 4.5
+            },
+            {
+                id: 6,
+                title: "5 Second Rule",
+                url: "assets/book-6.jpeg",
+                originalPrice: 47.95,
+                salePrice: 32.95,
+                rating: 4.5
+            },
+            {
+                id: 7,
+                title: "5 Second Rule",
+                url: "assets/book-6.jpeg",
+                originalPrice: 47.95,
+                salePrice: 32.95,
+                rating: 4.5
+            }
 
-    ]
+            ])
+        }, 1000);
+    });
 };
